eb088ca6e0c9d638543e36d28d68ac3c76b84b0c
Taingbunhoung <bunhong66@gmail.com>
2020-07-22 18:06:12 +0700

    Last Push

--- app/src/main/java/com/example/taingbunhoung/MainActivity.java
+++ app/src/main/java/com/example/taingbunhoung/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.taingbunhoung;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void ChangeFragment (View view){
+        Fragment fragment;
+
+        if ( view == findViewById(R.id.button)) {
+            fragment = new HomeFragment();
+
+            android.app.FragmentManager fm = getFragmentManager();
+            android.app.FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        if ( view == findViewById(R.id.button)) {
+            fragment = new Categlory();
+            android.app.FragmentManager fm = getFragmentManager();
+            android.app.FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        if ( view == findViewById(R.id.button)) {
+            fragment = new Search();
+            android.app.FragmentManager fm = getFragmentManager();
+            android.app.FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
+        if ( view == findViewById(R.id.button)) {
+            fragment = new Account();
+            android.app.FragmentManager fm = getFragmentManager();
+            android.app.FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.fragment_place, fragment);
+            ft.commit();
+        }
     }
 }
